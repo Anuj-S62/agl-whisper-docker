dd68e3725df3d15e4ab9708059432b70cc37474d
Anuj-S62 <anuj603362@gmail.com>
2024-06-30 20:16:18 +0530

    Update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,17 @@ WORKDIR /app
 
 # Install necessary system dependencies
 RUN apt-get update && \
-    apt-get install -y ffmpeg && \
+    apt-get install -y ffmpeg wget && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
 COPY . /app
 
 RUN pip install -r requirements.txt
+
+ENV MODEL_URL=https://openaipublic.azureedge.net/main/whisper/models/ed3a0b6b1c0edf879ad9b11b1af5a0e6ab5db9205f891f668f8b0e6c6326e34e/base.pt
+
+RUN wget -P ./models ${MODEL_URL}
 
 ENV CONFIG_PATH=/app/config.ini
 
--- agl_whisper_client.py
+++ agl_whisper_client.py
@@ -1,27 +1,3 @@
-# import grpc
-# from generated import audio_processing_pb2
-# from generated import audio_processing_pb2_grpc
-
-# def run():
-#     with grpc.insecure_channel('localhost:50051') as channel:
-#         stub = audio_processing_pb2_grpc.AudioProcessingStub(channel)
-        
-#         # Read audio file (must be 16kHz mono WAV file)
-#         with open('audio.wav', 'rb') as audio_file:
-#             audio_data = audio_file.read()
-                
-#         # Create a request
-#         request = audio_processing_pb2.AudioRequest(audio_data=audio_data)
-        
-#         # Send request and get response
-#         response = stub.ProcessAudio(request)
-        
-#         print(f"Received text: {response.text}")
-
-# if __name__ == '__main__':
-#     run()
-
-
 import grpc
 import argparse
 from generated import audio_processing_pb2
